007dde03acd09bf5eed5cd6baea73e0b6a45ae6d
n3ling <76264235+n3ling@users.noreply.github.com>
2024-09-18 17:44:29 +0000

    update get test to reflect new changes in db

--- tests/health.test.js
+++ tests/health.test.js
@@ -17,7 +17,7 @@ describe('CRUD Tests', () => {
         expect(res.body[0].givenName).toBe("John");
         expect(res.body[0].surname).toBe("Caruso");
         expect(res.body[0].email).toBe("John.D@email.com");
-        expect(res.body[0].employeeID).toBe(2);
+        expect(res.body[0].employeeID).toBe(7);
         const successResponse = JSON.parse(res.text);
         console.log(successResponse.length);
     });
