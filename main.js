a97fb87c4bdc0e5c7b6fdf26ae1081d2cb12b598
JTran001 <105179480+JTran001@users.noreply.github.com>
2024-12-05 21:11:49 -0500

    Removed Headers

--- main.js
+++ main.js
@@ -52,14 +52,6 @@ corsOption = {
 }
 app.use(cors(corsOption));
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  next();
-});
-
 // For highlighting active menu
 // ActiveRoute value = active route, eg, "/employees/add"
 app.use(function(req, res, next){
